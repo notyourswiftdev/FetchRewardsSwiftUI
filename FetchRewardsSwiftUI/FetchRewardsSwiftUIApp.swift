c0ae19757df1a0039749c927f7003ecb47482dc3
Aaron Cleveland <toscleveland@gmail.com>
2023-06-19 10:19:51 -0500

    Initial Commit

---
+++ FetchRewardsSwiftUI/FetchRewardsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRewardsSwiftUIApp.swift
+//  FetchRewardsSwiftUI
+//
+//  Created by Aaron Cleveland on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchRewardsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRewardsSwiftUIUITests/FetchRewardsSwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FetchRewardsSwiftUIUITestsLaunchTests.swift
+//  FetchRewardsSwiftUIUITests
+//
+//  Created by Aaron Cleveland on 6/19/23.
+//
+
+import XCTest
+
+final class FetchRewardsSwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
